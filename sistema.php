4668c48fbeb6b63ecef769ec19ca8064861cbe11
Giovani <gvntrck@gmail.com>
2025-10-08 16:17:54 -0300

    refactor: ajusta alinhamento e espaçamento do botão de adicionar celular usando classes Bootstrap

--- sistema.php
+++ sistema.php
@@ -1426,10 +1426,11 @@ $dashboard_stats = fetch_dashboard_stats();
                 </svg>
                 Auditoria
             </button>
-            <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#modalAdicionarCelular">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16" style="vertical-align: middle; margin-right: 4px;">
+            <button type="button" class="btn btn-primary btn-sm d-inline-flex align-items-center" data-bs-toggle="modal" data-bs-target="#modalAdicionarCelular">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="me-1" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
-                </svg>Adicionar Celular
+                </svg>
+                <span>Adicionar Celular</span>
             </button>
             <input id="search" type="search" class="form-control form-control-sm search-input" placeholder="Pesquisar...">
             <span class="badge bg-secondary">
