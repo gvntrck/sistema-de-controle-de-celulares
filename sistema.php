cd901367ea01f88a3c6ab29a4028a8d359b30b16
Giovani <gvntrck@gmail.com>
2025-10-08 11:48:57 -0300

    feat: adiciona tabela de auditoria no chip de informações do sistema

--- sistema.php
+++ sistema.php
@@ -1427,7 +1427,7 @@ $dashboard_stats = fetch_dashboard_stats();
                 Adicionar Celular
             </button>
             <input id="search" type="search" class="form-control form-control-sm search-input" placeholder="Pesquisar...">
-            <span class="chip">Tabelas: <?php echo esc($tables->celulares); ?>, <?php echo esc($tables->celulares_meta); ?>, <?php echo esc($tables->colaboradores); ?>, <?php echo esc($tables->colaboradores_meta); ?>, <?php echo esc($tables->transferencias); ?></span>
+            <span class="chip">Tabelas: <?php echo esc($tables->celulares); ?>, <?php echo esc($tables->celulares_meta); ?>, <?php echo esc($tables->colaboradores); ?>, <?php echo esc($tables->colaboradores_meta); ?>, <?php echo esc($tables->transferencias); ?>, <?php echo esc($tables->auditoria); ?></span>
             <span class="badge bg-secondary">
                 <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" viewBox="0 0 16 16" style="vertical-align: middle;">
                     <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
