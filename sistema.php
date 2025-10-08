41aea63771f6eb16bbd9749d7ca240bc75e763c3
Giovani <gvntrck@gmail.com>
2025-10-07 22:35:32 -0300

    refactor: simplifica mensagem de alerta no ambiente de testes

--- sistema.php
+++ sistema.php
@@ -484,7 +484,7 @@ $data = fetch_rows();
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-exclamation-triangle-fill me-2" viewBox="0 0 16 16" style="vertical-align: middle;">
             <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
         </svg>
-        <strong>Ambiente de Testes</strong> - Este sistema está em modo de testes. Os dados aqui inseridos são apenas para fins de demonstração.
+        <strong>Ambiente de Testes</strong>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Fechar"></button>
     </div>
     
